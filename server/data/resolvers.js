96daa6dfbb3f142ee22aa351c8eceba98c1f3fec
Mark Apinis <mapinis2019@gmail.com>
2017-12-10 11:52:56 -0500

    Formatting lmao

--- server/data/resolvers.js
+++ server/data/resolvers.js
@@ -1,19 +1,19 @@
-import db from './db.js';
+import db from "./db.js";
 
 const resolvers = {
   Query: {
     async tests(root, args, ctx) {
       // args: {id, url, isp}
-      const tests = await db('tests')
+      const tests = await db("tests")
         .where(args)
-        .select('*')
+        .select("*");
       return tests;
     },
     async average(root, args, ctx) {
       // args: {url}
       const query = await db.raw(
         `SELECT AVG(latencyavg) as latencyavg, url FROM tests WHERE url=:url GROUP BY url;`,
-        {url: args.url}
+        { url: args.url }
       );
       const result = query.rows[0];
       console.log(result);
@@ -28,13 +28,17 @@ const resolvers = {
         insert into tests as original ( url, isp, latencyavg)
         values ( :url, :isp, :latencyavg)
         returning *;
-        `
-      ,
-        {url: args.url, isp: args.isp, latencyavg: args.latencyavg }
+        `,
+        { url: args.url, isp: args.isp, latencyavg: args.latencyavg }
       );
 
       const record = result.rows[0];
-      return {id: record.id, url:record.url, isp:record.isp, latencyavg:record.latencyavg};
+      return {
+        id: record.id,
+        url: record.url,
+        isp: record.isp,
+        latencyavg: record.latencyavg
+      };
     }
   }
 };
