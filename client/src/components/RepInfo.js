0d3fe6365646e4595187346bbd34c25ff205bbca
AaronCGoidel <agoidel2019@mka.org>
2017-12-10 11:36:56 -0500

    not working function

--- client/src/components/RepInfo.js
+++ client/src/components/RepInfo.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
-export class RepInfo extends React.Component {
-  callApi(latitude, longitude){
+export default function callApi(latitude, longitude){
     fetch('https://openstates.org/api/v1/legislators/geo/?lat='+latitude+'&long='+longitude)
       .then((result) => {
         return result.json();
       }).then((jsonResult) => {
       console.log(jsonResult);
-    })
-  }
+    });
+  return null;
 }
-
-export default RepInfo
