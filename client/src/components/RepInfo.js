beedbd868c396ee3b000b12c78ac3de6642b7adf
AaronCGoidel <agoidel2019@mka.org>
2017-12-10 11:24:59 -0500

    callAPI function

--- client/src/components/RepInfo.js
+++ client/src/components/RepInfo.js
@@ -1,6 +1,6 @@
 export class RepInfo extends React.Component {
-  callApi(){
-    fetch('https://openstates.org/api/v1/legislators/?state=ny')
+  callApi(latitude, longitude){
+    fetch('https://openstates.org/api/v1/legislators/geo/?lat='+latitude+'&long='+longitude)
       .then((result) => {
         return result.json();
       }).then((jsonResult) => {
