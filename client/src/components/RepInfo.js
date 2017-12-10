ca8b6e9e733ab79b573f67c9496e0833a35d81bc
AaronCGoidel <agoidel2019@mka.org>
2017-12-10 11:55:47 -0500

    filter name and email

--- client/src/components/RepInfo.js
+++ client/src/components/RepInfo.js
@@ -5,6 +5,9 @@ export default async function callApi(latitude, longitude) {
     'https://openstates.org/api/v1/legislators/geo/?lat=' + latitude + '&long=' + longitude
   );
   const json = await result.json();
-  console.log(json);
+  const fistName = json.first_name;
+  const lastName = json.last_name;
+  const email = json.email;
+  console.log(fistName + " " + lastName + ": " + email);
   return null;
 }
