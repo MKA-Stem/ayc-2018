aacecda5db123e4a0b9d23ffd7cfd82505d1fa3f
William Goodall <wgoodall01@gmail.com>
2017-12-10 12:52:26 -0500

    Tester red/green

--- client/src/components/TestStatus.js
+++ client/src/components/TestStatus.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import LoadingSpinner from 'components/LoadingSpinner.js';
 import './TestStatus.css';
+import classnames from 'classnames';
 
 const TestStatus = ({url, result, avg}) => {
-  const percent = result / (avg || result);
+  const ratio = 1 - result / (avg || result);
+
+  let color = null;
+
+  const percent = ratio * 100;
+  const formatted = `${Math.floor(Math.abs(percent))}%`;
+
+  if (percent < -30) {
+    color = 'red';
+  } else if (percent > 30) {
+    color = 'green';
+  }
+
+  const cls = classnames(
+    'TestStatus',
+    {'TestStatus_color-green': color === 'green'},
+    {'TestStatus_color-red': color === 'red'}
+  );
 
   return (
-    <div className="TestStatus">
+    <div className={cls}>
       <div className="TestStatus_url">{url}</div>
-      {result == null ? <LoadingSpinner /> : <div className="TestStatus_latency">{percent}</div>}
+      {result == null ? (
+        <LoadingSpinner />
+      ) : (
+        <div className="TestStatus_latency">
+          {formatted}
+          {percent > 0 ? ' faster' : ' slower'}
+        </div>
+      )}
     </div>
   );
 };
