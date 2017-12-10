24a466c2cbfe09520884b1e185fcdefcd46e951e
Mark Apinis <mapinis2019@gmail.com>
2017-12-10 13:16:56 -0500

    repInfo fixing and getISPLoc formatting

--- client/src/lib/getISPLoc.js
+++ client/src/lib/getISPLoc.js
@@ -1,5 +1,5 @@
 export async function getISPLoc() {
-    const response = await fetch("http://ip-api.com/json");
-    const ans = await response.json();
-    return {isp: ans.isp, latitude: ans.lat, longitude: ans.lon};
+  const response = await fetch("http://ip-api.com/json");
+  const ans = await response.json();
+  return { isp: ans.isp, latitude: ans.lat, longitude: ans.lon };
 }
---
+++ client/src/lib/repInfo.js
@@ -0,0 +1,21 @@
+export async function callApi(latitude, longitude) {
+  const response = await fetch(
+    "https://openstates.org/api/v1/legislators/geo/?lat=" +
+      latitude +
+      "&long=" +
+      longitude +
+      "&apikey=cf0a3c81-4d18-4a7b-a8b7-db9d2a299094"
+  );
+  const reps = await response.json();
+  const result = {};
+  for (let rep of reps) {
+    result[rep.chamber] = {
+      lastName: rep.last_name,
+      firstName: rep.first_name,
+      contact: rep.offices[0].phone
+        ? rep.offices[0].phone
+        : rep.offices[0].email
+    };
+  }
+  return result;
+}
