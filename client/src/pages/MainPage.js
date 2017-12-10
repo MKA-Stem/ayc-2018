f4764096ac6dc9308f1396453e8922bb541817a4
AaronCGoidel <agoidel2019@mka.org>
2017-12-10 13:30:38 -0500

    about nettest

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -49,6 +49,17 @@ const MainPage = ({history}) => (
         Imagine if you had to pay $20 extra to have “Youtube Priority Access”. And that without it, you are
         throttled into watching videos at a meager 240p.
       </p>
+
+      <h2>What is NetTest?</h2>
+      <p>
+        NetTest is a distributed latency testing service. We ping some of the most popular websites on
+        the internet and compare your results to a global average.
+      </p>
+      <p>
+        This score will give you a sense of where your internet service provider is throttling your connection. Plus,
+        if you don't like what you see, we will give you the information you need to contact your representatives in
+        congress so you can bring this to their attention.
+      </p>
     </div>
   </div>
 );
