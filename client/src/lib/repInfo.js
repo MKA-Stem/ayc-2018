b1da8cad48aa5027316f019a3436b78bd50c63eb
Mark Apinis <mapinis2019@gmail.com>
2017-12-10 23:30:14 -0500

    Formatting

--- client/src/lib/repInfo.js
+++ client/src/lib/repInfo.js
@@ -1,10 +1,15 @@
 export async function repInfo(state) {
   const response = await fetch(
-    'https://api.propublica.org/congress/v1/members/senate/' +
+    "https://api.propublica.org/congress/v1/members/senate/" +
       state +
-      '/current.json'
-  , {mode:'cors', headers: {'X-API-Key':'HYJ3Bckf6gusHxa6eQAc7GPP875hlFALzRUeOsFs'}});
-  let reps = await response.json(); reps = reps.results;
+      "/current.json",
+    {
+      mode: "cors",
+      headers: { "X-API-Key": "HYJ3Bckf6gusHxa6eQAc7GPP875hlFALzRUeOsFs" }
+    }
+  );
+  let reps = await response.json();
+  reps = reps.results;
   const ids = [];
   for (let rep of reps) {
     ids.push(rep.id);
@@ -13,15 +18,19 @@ export async function repInfo(state) {
   const results = [];
   for (let id of ids) {
     const senatorResponse = await fetch(
-      'https://api.propublica.org/congress/v1/members/' + id + '.json'
-    , {mode:'cors', headers: {'X-API-Key':'HYJ3Bckf6gusHxa6eQAc7GPP875hlFALzRUeOsFs'}});
+      "https://api.propublica.org/congress/v1/members/" + id + ".json",
+      {
+        mode: "cors",
+        headers: { "X-API-Key": "HYJ3Bckf6gusHxa6eQAc7GPP875hlFALzRUeOsFs" }
+      }
+    );
     let senatorInfo = await senatorResponse.json();
     senatorInfo = senatorInfo.results[0];
-    
+
     results.push({
-      firstName:senatorInfo.first_name,
-      lastName:senatorInfo.last_name,
-      contact:senatorInfo.roles[0].phone
+      firstName: senatorInfo.first_name,
+      lastName: senatorInfo.last_name,
+      contact: senatorInfo.roles[0].phone
     });
   }
   return results;
